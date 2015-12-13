60671a4a2fef54768fbd4959f95d05baefe24e17
Tim <timrobertson0122@gmail.com>
2015-12-13 20:52:02 +0100

    added testing suite, dummy feature test passing

---
+++ spec/features/urlShortenerFeature.js
@@ -0,0 +1,37 @@
+var webdriverio = require('webdriverio');
+var expect = require('chai').expect;
+var shortener = require('../../routes/urlShortener');
+
+describe('Url Shortener', function() {
+
+	var client = {};
+
+	before(function(done) {
+		client = webdriverio.remote({ desiredCapabilities: {browserName: 'chrome'} });
+		client.init(done);
+	});
+
+	after(function(done) {
+		client.end(done);
+	});
+
+	it('displays the title', function(done) {
+		client
+			.url('http://localhost:3000')
+			.getText('body', function(err, text) {
+				expect(err).to.not.be.true;
+				expect(text).to.equal('Welcome to Tim.ly - the URL Shortener :)');
+			})
+			.call(done);
+	});
+
+	it('can create and return a shortened url', function(done) {
+		var originalUrl = 'www.github.com/timrobertson0122/URLshortener';
+		client
+			.url('http://localhost:3000')
+			.click("shorten", done);
+
+			expect(shortUrl).to.not.equal('');
+	});
+
+});
